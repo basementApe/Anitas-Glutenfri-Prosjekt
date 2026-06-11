cb00c825e00d5bcc767d744bad393b8d616c7311
basementApe <71020874+basementApe@users.noreply.github.com>
2026-06-11 12:22:31 +0200

    Update viewRegisterUserPage.js

--- registerUserPage/viewRegisterUserPage.js
+++ registerUserPage/viewRegisterUserPage.js
@@ -8,10 +8,10 @@ function confirmPassword(userInput)
 function viewRegisterUserPage()
 {
     app.innerHTML = /*html*/ `
-        <div class="containerLogin" style="background-image: url('/Anitas%20Glutenfri%20Prosjekt/media/background/background.jpg');">
+        <div class="containerLogin" style="background-image: url('media/background/background.jpg');">
 
             <button type="button" class="loginBackButton" onclick="model.app.currentPage='frontPage'; updateView();">
-                <img src="/Anitas%20Glutenfri%20Prosjekt/media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
+                <img src="media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
            </button>
         <div>
 
