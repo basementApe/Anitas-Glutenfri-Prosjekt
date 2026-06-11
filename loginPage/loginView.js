749a16bb466d664afb470f75fe42770a6af71539
basementApe <71020874+basementApe@users.noreply.github.com>
2026-06-11 09:50:08 +0200

    relative path fix

--- loginPage/loginView.js
+++ loginPage/loginView.js
@@ -32,10 +32,10 @@ function viewLogInElements()
 function viewLoginPage() 
 {
     app.innerHTML = `
-        <div class="containerLogin" style="background-image: url('/Anitas%20Glutenfri%20Prosjekt/media/background/background.jpg');">
+        <div class="containerLogin" style="background-image: url('media/background/background.jpg');">
 
             <button type="button" class="loginBackButton" onclick="model.app.currentPage='frontPage'; updateView();">
-                <img src="/Anitas%20Glutenfri%20Prosjekt/media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
+                <img src="media/icons/login/arrow_back_ios_new.svg" alt="" width="16" height="16" aria-hidden="true">
            </button>
 
             ${ model.app.adminLoggedIn ? viewLoggedInAdmin() : viewLogInElements() }
